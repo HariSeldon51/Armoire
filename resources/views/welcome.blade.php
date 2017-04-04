cf740d6ded16b96f9dc7d14ecbe74f2bb0883fe9
Will DeHaven <dehavenbooks@gmail.com>
2017-04-03 22:01:54 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Idex Solutions Client Access | pre-Alpha</title>
+        <title>Armoire</title>
         <!-- The normalizing css stylesheet -->
         <link rel="stylesheet" href="css/normalize.min.css">
         
@@ -50,7 +50,7 @@
         
         <paper-toolbar id="toolbar">
 
-            <paper-item>Portex - The flexible web portal solution</paper-item>
+            <paper-item>Armoire - The flexible web portal solution</paper-item>
             <span class="flex"></span>
 
             @if (Auth::guest())
