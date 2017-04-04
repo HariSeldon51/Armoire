d1642b4c30c5b4ea9708012dd87163fd91510959
Will DeHaven <dehavenbooks@gmail.com>
2017-04-04 00:38:02 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
             }
 
             #nav-drawer {
-                background-image: url('img/NavDrawerBG.png');
+                background-image: url('img/navdrawerBG.png');
                 color: #ffffff;
                 
                 height: 225px;
