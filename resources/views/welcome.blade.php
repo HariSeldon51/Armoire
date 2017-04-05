80746878c3e604b1c58ec921fc8dd858e00a6841
BuildTools <unconfigured@null.spigotmc.org>
2017-04-04 21:56:18 -0700

    Extended app layout in welcome view
    Changed the 'welcome' view to extend the default app blade layout,
    rather than implement the layout and themese on its own.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,57 +1,6 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-    
-    <head>
+@extends('layouts.app')
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Armoire</title>
-        <!-- The normalizing css stylesheet -->
-        <link rel="stylesheet" href="css/normalize.min.css">
-        
-        <!-- The main css stylesheet -->
-        <link rel="stylesheet" href="css/main.css">
-
-        <!-- Modernizr script import -->
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-        
-        <!-- Google Polymer import -->
-        <link href="bower_components/polymer/polymer.html" rel="import">
-        
-        <!-- Element Imports -->
-        <link href="bower_components/paper-toolbar/paper-toolbar.html" rel="import">
-        <link href="bower_components/paper-item/paper-item.html" rel="import">
-        <link href="bower_components/paper-button/paper-button.html" rel="import">
-        
-        <!-- Scripts -->
-        <script>
-            window.Laravel = <?php echo json_encode([
-                'csrfToken' => csrf_token(),
-            ]); ?>
-        </script>
-        
-        <style>
-        
-            #toolbar {
-               background-color: #005736; 
-            }
-            
-            a {
-                color: white;
-            }
-        
-        </style>
-        
-    </head>
-    <body>
-        
-        <paper-toolbar id="toolbar">
-
-            <paper-item>Armoire - The flexible web portal solution</paper-item>
-            <span class="flex"></span>
+@section('content')
 
             @if (Auth::guest())
             <a href="{{ url('/login') }}" tabindex="-1">
@@ -65,13 +14,5 @@
                 <paper-button onclick="">Home</paper-button>
             </a>
             @endif
-            
-        </paper-toolbar>
-            
-        <div class="content">
-            <div>
-            </div>
-        </div>
-        
-    </body>
-</html>
+
+@endsection
