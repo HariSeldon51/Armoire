5fd1772f6d22f87c8c2e6402061dec252b20bdff
BuildTools <unconfigured@null.spigotmc.org>
2017-04-04 23:36:34 -0700

    Additional instructions
    Added additional instructions regarding the registration page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
             
             <div class="card-content">Armoire is the elegant, modular, and customizable web portal. Try out this demo of Armoire by logging-in as an admininstrator. Login details are provided on the login page.</div>
             
+            <div class="card-content">The registration page will allow you to register a user, but no registered users are currently allowed to log in except the demo administrator account.</div>
+            
             <div class="card-actions">
                 <paper-button onclick="gotoPage(this, '/login')">Login</paper-button>
                 <paper-button class="loud" onclick="gotoPage(this, '/register')">Register</paper-button>
